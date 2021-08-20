4e5524548ddb46ed805f39f235ec3ce4474fdead
Max Garro Mora <maxgarro22@gmail.com>
2021-08-19 21:56:30 -0600

    [Update] Quick Sort
    Se agrego la clase QS y sus métodos

---
+++ QuickSort.h
@@ -0,0 +1,17 @@
+//
+// Created by maxga on 17/08/2021.
+//
+
+#ifndef TAREA1_QUICKSORT_H
+#define TAREA1_QUICKSORT_H
+using namespace std;
+
+class QuickSort {
+    public:
+        static void printArray(int *, int );
+        static void quicksort(int *, int , int );
+
+};
+
+
+#endif //TAREA1_QUICKSORT_H
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "QuickSort.h"
+
+
+int main()
+{
+    int array[] = {95, 45, 48, 98, 1, 485, 65, 478, 1, 2325};
+    int n = sizeof(array)/sizeof(array[0]);
+
+    std::cout << "Before Quick Sort :" << std::endl;
+    QuickSort::printArray(array, n);
+    
+    QuickSort::quicksort(array, 0, n);
+
+    std::cout << "After Quick Sort :" << std::endl;
+    QuickSort::printArray(array, n);
+    return (0);
+}
